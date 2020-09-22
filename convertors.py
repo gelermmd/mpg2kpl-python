c89e0be91c3085e228a88842deac5ad7b888046b
Gelermmd <gelermmd@dukes.jmu.edu>
2020-09-22 10:47:23 -0700

    first working version

--- convertors.py
+++ convertors.py
@@ -1,1 +1,5 @@
-print(25*1.609344*0.2641720524)
+#ask the user to input an MPG value
+mpg = input("What is the MPG?")
+#convert the input to a numeric (float) value
+mpg = float(mpg)
+print(mpg*1.609344*0.2641720524)
